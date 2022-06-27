ff1150340800f0b829bda326ba7b187b082b3333
Agustin Serrano Cucarella <agserran@c3r6s6.42madrid.com>
2022-06-27 18:07:13 +0200

    libft

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ ft_isascii.c \
 ft_isdigit.c \
 ft_isprint.c \
 ft_memchr.c \
-ft_memcmp.c \
+ft_memcmp.c \
 ft_memcpy.c \
 ft_memmove.c \
 ft_memset.c \
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: agserran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 18:22:26 by agserran          #+#    #+#             */
-/*   Updated: 2022/06/22 18:31:34 by agserran         ###   ########.fr       */
+/*   Updated: 2022/06/27 18:05:02 by agserran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ void	*ft_memmove(void *dst, const void *src, size_t n)
 	o = (unsigned char *)dst;
 	if (d > o)
 	{
-		while (len--)
-			d[len] = o[len];
+		while (n--)
+			d[n] = o[n];
 	}
 	else if (d < o)
-		ft_memcpy(d, s, len);
+		ft_memcpy(d, o, n);
 	return (d);
 }
