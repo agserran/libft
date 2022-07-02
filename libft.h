33f60d4e39cef55afc9bf99bd4749f8005495dc7
agserran <agustinsscc01@gmail.com>
2022-07-02 18:01:21 +0200

    2

--- libft.h
+++ libft.h
@@ -34,5 +34,7 @@ int			ft_toupper(int c);
 char        *ft_strchr(const char *s, int c);
 char        *ft_strrchr(const char *s, int c);
 int         ft_strncmp(const char *s1, const char *s2, size_t n);
+void        *ft_memchr(const void *s, int c, size_t n);
+int         memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
