3d897f1093d0c0fd838e5cc24e5c419721d5780c
agserran <agustinsscc01@gmail.com>
2022-07-21 13:43:06 +0200

    libft

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -12,7 +12,7 @@
 
 #include "libft.h"
 
-int	ft_isalnum(int c)
+int	 ft_isalnum(int c)
 {
 	if ((c >= '0' && c <= '9') || (c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
--- ft_memset.c
+++ ft_memset.c
@@ -12,7 +12,7 @@
 
 #include "libft.h"
 
-void	*ft_memset(void *s, int c, size_t n)
+void	 *ft_memset(void *s, int c, size_t n)
 {
 	size_t	x;
 
